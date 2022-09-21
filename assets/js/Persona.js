81c8eb0c50be3ff841a324318eed9549a9121672
kodigo-denny <denny.herrera@kodigo.org>
2022-09-20 23:17:42 -0600

    Cambios Semana 7 dia 2

---
+++ assets/js/Persona.js
@@ -0,0 +1,17 @@
+class Persona{
+    dui// ID
+    nombre
+    apellido
+    direccion
+    telefono
+    correo
+
+    constructor(dui, nombre, apellido, direccion, telefono, correo){
+        this.dui = dui;
+        this.nombre = nombre,
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+}
